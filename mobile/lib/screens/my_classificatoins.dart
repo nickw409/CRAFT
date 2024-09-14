5c975cc67c9f8fad7e401c2497088a70f1a55f67
Aadarsha Bastola <aadarsha.com@gmail.com>
2024-09-14 06:21:26 -0700

    UI change

--- mobile/lib/screens/my_classificatoins.dart
+++ mobile/lib/screens/my_classificatoins.dart
@@ -145,19 +145,16 @@ class _MyClassificationsState extends State<MyClassifications> {
         child: Scaffold(
           appBar: AppBar(
             centerTitle: false,
-            toolbarHeight: 100,
+            toolbarHeight: 80,
             automaticallyImplyLeading: false,
             title: const Padding(
               padding: EdgeInsets.all(8.0),
-              child: Padding(
-                padding: EdgeInsets.only(top: 32),
-                child: Text(
-                  'History',
-                  style: TextStyle(
-                      fontFamily: 'Uber',
-                      fontSize: 60,
-                      fontWeight: FontWeight.w700),
-                ),
+              child: Text(
+                'History',
+                style: TextStyle(
+                    fontFamily: 'Uber',
+                    fontSize: 60,
+                    fontWeight: FontWeight.w700),
               ),
             ),
             bottom: const TabBar(
