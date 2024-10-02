aed01d7badeac928aa8e714d3c8fae4fb94c440f
nickw409 <nwiley333@gmail.com>
2024-10-02 10:02:35 -0700

    Creating the model

--- ai/src/models/swinTransformer.py
+++ ai/src/models/swinTransformer.py
@@ -381,3 +381,27 @@ test_dataset = (
 
 # Building the model here
 input = layers.Input(shape=(256, 12))
+x = PatchEmbedding(num_patch_x * num_patch_y, embed_dim)(input)
+x = SwinTransformer(
+    dim=embed_dim,
+    num_patch=(num_patch_x, num_patch_y),
+    num_heads=num_heads,
+    window_size=window_size,
+    shift_size=0,
+    num_mlp=num_mlp,
+    qkv_bias=qkv_bias,
+    dropout_rate=dropout_rate,
+)(x)
+x = SwinTransformer(
+    dim=embed_dim,
+    num_patch=(num_patch_x, num_patch_y),
+    num_heads=num_heads,
+    window_size=window_size,
+    shift_size=shift_size,
+    num_mlp=num_mlp,
+    qkv_bias=qkv_bias,
+    dropout_rate=dropout_rate,
+)(x)
+x = PatchMerging((num_patch_x, num_patch_y), embed_dim=embed_dim)(x)
+x = layers.GlobalAveragePooling1D()(x)
+output = layers.Dense(num_classes, activation="softmax")(x)
