fd7e13b14d84ed8ad317bc8ea0b0e9e8b86176de
nickw409 <nwiley333@gmail.com>
2024-10-02 07:49:43 -0700

    Setting foundation for keras swin transformer example

---
+++ ai/src/models/swinTransformer.py
@@ -0,0 +1,13 @@
+"""
+This is a recreation of the Swin Transformer example found at
+https://keras.io/examples/vision/swin_transformers/ that will be the
+basis for our implementation of the Swin Transformer.
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import keras
+from keras import layers
+from keras import ops
